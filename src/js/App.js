437c8baa5504ac2ec6259acef9bb18892e4e8084
Sergey Nagornykh <serega1905@ukr.net>
2023-12-15 20:15:05 +0200

    app base structure

---
+++ src/js/App.js
@@ -0,0 +1,5 @@
+export class App {
+  render(appNode) {
+    appNode.innerHTML = `<h1>Hello Vanilla JS APP</h1>`;
+  }
+}
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import {App} from "./js/App.js";
+
+const app = new App();
+
+app.render(document.querySelector('#app'));
